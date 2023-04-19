930842998b4afe680d07a9429dab13f122888f03
lk <lk-mt-sc@proton.me>
2023-04-19 17:50:55 +0200

    Add status manager

---
+++ infer_pipeline/manager/status_manager.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+
+class Status(Enum):
+    IDLE = 'Idle'
+    FETCHING_DATA = 'Fetching data'
+    FETCHING_MODELS = 'Fetching models'
+    FETCHING_INFERENCES = 'Fetching inferences'
+    INFERING = 'Infering'
+    LOADING_IMAGE = 'Loading image'
+
+
+class StatusManager():
+    def __init__(self, gui_status):
+        self.gui_status = gui_status
+        self.status = []
+        self._set_status()
+        self._set_progress()
+
+    def add_status(self, status):
+        self.status.append(status)
+        self._set_status()
+
+    def remove_status(self, status):
+        self.status.remove(status)
+        self._set_status()
+
+    def has_status(self, status):
+        return status in self.status
+
+    def update_progress(self, value):
+        self._set_progress(value)
+
+    def _set_status(self):
+        if not self.status:
+            self.status.append(Status.IDLE)
+
+        if len(self.status) > 1 and Status.IDLE in self.status:
+            self.status.remove(Status.IDLE)
+
+        status_str = [status.value for status in self.status]
+        self.gui_status.status_var.set(' | '.join(status_str))
+
+    def _set_progress(self, value=0):
+        self.gui_status.progressbar['value'] = value
