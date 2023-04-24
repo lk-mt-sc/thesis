8f85c7b06ad87c274429c02dcd3707be8814959a
lk <lk-mt-sc@proton.me>
2023-04-24 19:49:09 +0200

    Add more commonly used constant paths

--- infer_pipeline/common.py
+++ infer_pipeline/common.py
@@ -5,8 +5,13 @@ INFERENCES_DIR = os.path.join(WORKING_DIR, 'inferences')
 
 MMPOSE_DIR = os.environ['MMPOSE_DIR']
 MMPOSE_DATA_DIR = os.path.join(MMPOSE_DIR, 'data', 'sc')
+MMPOSE_RUNS_DIR = os.path.join(MMPOSE_DATA_DIR, 'runs')
+MMPOSE_DATASET_DIR = os.path.join(MMPOSE_DATA_DIR, 'dataset')
 MMPOSE_CHECKPOINTS_DIR = os.path.join(MMPOSE_DIR, 'checkpoints')
+MMPOSE_TEST_SCRIPT = os.path.join(MMPOSE_DIR, 'tools', 'test.py')
 
 MMDETECTION_DIR = os.environ['MMDETECTION_DIR']
 MMDETECTION_DATA_DIR = os.path.join(MMDETECTION_DIR, 'data', 'sc')
 MMDETECTION_CHECKPOINTS_DIR = os.path.join(MMDETECTION_DIR, 'checkpoints')
+MMDETECTION_TEST_SCRIPT = os.path.join(MMDETECTION_DIR, 'tools', 'test.py')
+MMDETECTION_IMAGES2COCO_SCRIPT = os.path.join(MMDETECTION_DIR, 'tools', 'dataset_converters', 'images2coco.py')
