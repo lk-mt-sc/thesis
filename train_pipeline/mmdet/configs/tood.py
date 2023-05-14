fd2346358cbc9df46e3c881f2ce5478d9232a40a
lk <lk-mt-sc@proton.me>
2023-05-14 17:45:08 +0200

    Add MMDetection training pipeline

---
+++ train_pipeline/mmdet/configs/tood.py
@@ -0,0 +1,217 @@
+# model
+model = dict(
+    type='TOOD',
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32),
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101'),
+        dcn=dict(type='DCNv2', deformable_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True)),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_output',
+        num_outs=5),
+    bbox_head=dict(
+        type='TOODHead',
+        num_classes=1,
+        in_channels=256,
+        stacked_convs=6,
+        feat_channels=256,
+        anchor_type='anchor_free',
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            ratios=[1.0],
+            octave_base_scale=8,
+            scales_per_octave=1,
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[0.1, 0.1, 0.2, 0.2]),
+        initial_loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            activated=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_cls=dict(
+            type='QualityFocalLoss',
+            use_sigmoid=True,
+            activated=True,
+            beta=2.0,
+            loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
+        num_dcn=2),
+    train_cfg=dict(
+        initial_epoch=4,
+        initial_assigner=dict(type='ATSSAssigner', topk=9),
+        assigner=dict(type='TaskAlignedAssigner', topk=13),
+        alpha=1,
+        beta=6,
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.6),
+        max_per_img=100))
+
+# dataset
+dataset_type = 'CocoDataset'
+data_root = 'data/sc/det/'
+
+# pipelines
+backend_args = None
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='RandomResize', scale=[(1333, 480), (1333, 800)],
+        keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+# dataloaders
+metainfo = dict(classes=('climber', ), palette=[(220, 20, 60)])
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/train.json',
+        data_prefix=dict(img='train/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args,
+        metainfo=metainfo))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/val.json',
+        data_prefix=dict(img='val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+        metainfo=metainfo))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/test.json',
+        data_prefix=dict(img='test/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+        metainfo=metainfo))
+
+# evaluators
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file='data/sc/det/annotations/val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file='data/sc/det/annotations/test.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
+
+# loop configs
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=300, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate scheduler
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=24,
+        by_epoch=True,
+        milestones=[16, 22],
+        gamma=0.1)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
+auto_scale_lr = dict(enable=False, base_batch_size=16)
+
+# hooks
+default_scope = 'mmdet'
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=32),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook',
+        interval=1,
+        max_keep_ckpts=1,
+        save_last=False,
+        save_best='auto',
+        rule='greater'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='DetVisualizationHook'))
+
+# runtime configs
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend')],
+    name='visualizer')
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+log_level = 'INFO'
+load_from = 'checkpoints/tood_r101_fpn_dconv_c3-c5_mstrain_2x_coco_20211210_213728-4a824142.pth'
+randomness = dict(seed=0)
---
+++ train_pipeline/mmdet/pipeline.py
@@ -0,0 +1,169 @@
+import os
+import re
+import time
+import glob
+import logging
+import argparse
+import subprocess
+from collections import deque
+from datetime import datetime
+from datetime import timedelta
+
+if __name__ == '__main__':
+    WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
+    CONFIGS_DIR = os.path.join(WORKING_DIR, 'configs')
+    TRAININGS_DIR = os.path.join(WORKING_DIR, 'trainings')
+    MMDETECTION_DIR = os.environ['MMDETECTION_DIR']
+    MMDETECTION_TRAIN_SCRIPT = os.path.join(MMDETECTION_DIR, 'tools', 'train.py')
+    MMDETECTION_TEST_SCRIPT = os.path.join(MMDETECTION_DIR, 'tools', 'test.py')
+
+    configs_all = [
+        os.path.join(CONFIGS_DIR, 'faster-rcnn.py'),
+        os.path.join(CONFIGS_DIR, 'rtmdet.py'),
+        os.path.join(CONFIGS_DIR, 'tood.py'),
+        os.path.join(CONFIGS_DIR, 'vfnet.py'),
+        os.path.join(CONFIGS_DIR, 'yolox.py'),
+    ]
+
+    default_training_name = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--training-name', type=str, default=default_training_name, required=False)
+    parser.add_argument('--configs', nargs='+', type=str, default='all', required=False)
+    parser.add_argument('--patience', type=int, default=10, required=False)
+    parser.add_argument('--resume', action='store_true')
+    args = parser.parse_args()
+
+    training_name = args.training_name
+    patience = args.patience
+    resume = args.resume
+
+    training_dir = os.path.join(TRAININGS_DIR, training_name)
+    if not os.path.exists(training_dir):
+        os.mkdir(training_dir)
+    else:
+        assert resume, f'A training session with name {training_name} already exists, '\
+            'it can be resumed using --resume argument. If it should not be resumed, '\
+            'please use another training session name.'
+
+    configs = []
+    if args.configs != 'all':
+        for config in args.configs:
+            config_path = os.path.join(WORKING_DIR, config)
+            configs.append(config_path)
+    else:
+        configs = configs_all.copy()
+
+    for config in configs:
+        assert os.path.exists(config), f'{config} configuration not found.'
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    log_file = os.path.join(training_dir, training_name + '.log')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    logger.info(f'Start training session: {training_name}.')
+
+    n_configs = len(configs)
+    for i, config in enumerate(configs):
+        base_name = config.split('/')[-1].split('.')[0]
+        train_work_dir = os.path.join(training_dir, base_name, 'train')
+        test_work_dir = os.path.join(training_dir, base_name, 'test')
+
+        training_args = [
+            'python',
+            '-W',
+            'ignore',
+            MMDETECTION_TRAIN_SCRIPT,
+            config,
+            '--work-dir',
+            train_work_dir,
+        ]
+
+        if resume:
+            training_args.append('--resume')
+            logger.info(f'Resume training of configuration: {config} ({i+1}/{n_configs}).')
+        else:
+            logger.info(f'Start training of configuration: {config} ({i+1}/{n_configs}).')
+
+        start = time.time()
+        training = subprocess.Popen(
+            training_args,
+            cwd=MMDETECTION_DIR,
+            stdout=subprocess.PIPE,
+            bufsize=1,
+            universal_newlines=True
+        )
+
+        val_results = deque(maxlen=patience)
+        best_result = 0
+        while True:
+            line = training.stdout.readline()
+            if not line:
+                print()
+                break
+            line = line.rstrip()
+            if 'Epoch(train)' in line:
+                log_str = line[line.find('Epoch(train)'):]
+                logger.info(log_str)
+            if 'coco/bbox_mAP' in line:
+                log_str = line[line.find('Epoch(val)'):]
+                logger.info(log_str)
+                result = float(re.findall("[+-]?\d+\.\d+", line)[0])
+                if result > best_result:
+                    best_result = result
+                val_results.append(result)
+                saturated = all(i < best_result for i in val_results)
+                if saturated:
+                    end = time.time()
+                    duration = end - start
+                    log_str = f'Stop training due to saturated validation set bbox_mAP (patience={patience}). '
+                    log_str += f'Train duration: {timedelta(seconds=duration)}'
+                    logger.info(log_str)
+                    training.terminate()
+
+        best_checkpoint = glob.glob(os.path.join(train_work_dir, 'best*.pth'))[0]
+        best_epoch = int(best_checkpoint.split('/')[-1].split('.')[0].split('_')[-1])
+        logger.info(f'Evaluate test set on checkpoint {best_checkpoint} of best epoch {best_epoch}.')
+
+        testing_args = [
+            'python',
+            '-W',
+            'ignore',
+            MMDETECTION_TEST_SCRIPT,
+            config,
+            best_checkpoint,
+            '--work-dir',
+            test_work_dir,
+        ]
+
+        testing = subprocess.Popen(
+            testing_args,
+            cwd=MMDETECTION_DIR,
+            stdout=subprocess.PIPE,
+            bufsize=1,
+            universal_newlines=True
+        )
+
+        while True:
+            line = testing.stdout.readline()
+            if not line:
+                print()
+                break
+            line = line.rstrip()
+            if 'coco/bbox_mAP' in line:
+                log_str = line[line.find('Epoch(test)'):]
+                logger.info(log_str)
+
+        if i != n_configs - 1:
+            logger.info('.')
