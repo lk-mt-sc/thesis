aabd0b16770a0a09131c463f86df4a22ce182b3b
lk <lk-mt-sc@proton.me>
2023-06-14 17:50:52 +0200

    Add support for transfer learned pose estimation models

--- infer_pipeline/data_types/mmpose_model.py
+++ infer_pipeline/data_types/mmpose_model.py
@@ -1,5 +1,5 @@
 class MMPoseModel():
-    def __init__(self, section, arch, dataset, input_size, key_metric, checkpoint, config):
+    def __init__(self, section, arch, dataset, input_size, key_metric, checkpoint, config, transfer_learned=False):
         self.section = section
         self.arch = arch
         self.dataset = dataset
@@ -7,27 +7,30 @@ class MMPoseModel():
         self.key_metric = key_metric
         self.checkpoint = checkpoint
         self.config = config
+        self.transfer_learned = transfer_learned
 
     @classmethod
     def get_from_selection_string(cls, selection_str):
         selection_str_split = selection_str.split(' | ')
         return MMPoseModel(
-            section=selection_str_split[3],
-            arch=selection_str_split[4],
-            dataset=selection_str_split[0],
-            input_size=selection_str_split[2],
-            key_metric=selection_str_split[1],
+            section=selection_str_split[4],
+            arch=selection_str_split[5],
+            dataset=selection_str_split[1],
+            input_size=selection_str_split[3],
+            key_metric=selection_str_split[2],
             checkpoint=None,
-            config=None
+            config=None,
+            transfer_learned=selection_str_split[0] == 'TL: True'
         )
 
     def __str__(self):
-        return f'{self.dataset} | {self.key_metric} | {self.input_size} | {self.section} | {self.arch}'
+        return f'TL: {str(self.transfer_learned)} | {self.dataset} | {self.key_metric} | {self.input_size} | {self.section} | {self.arch}'
 
     def __eq__(self, other):
         equal = self.section == other.section \
             and self.arch == other.arch \
             and self.dataset == other.dataset \
             and self.input_size == other.input_size \
-            and self.key_metric == other.key_metric
+            and self.key_metric == other.key_metric \
+            and self.transfer_learned == other.transfer_learned
         return equal
