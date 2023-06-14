4b1dae2e5d8f2cdc7dd46a02dbfc0f5bdd65bb59
lk <lk-mt-sc@proton.me>
2023-06-14 17:48:22 +0200

    Add default COCO keypoints

--- infer_pipeline/manager/dataset_manager.py
+++ infer_pipeline/manager/dataset_manager.py
@@ -8,6 +8,26 @@ class Datasets(Enum):
     COCO = 'coco'
 
 
+class KeypointsDefaultCOCO(Enum):
+    NOSE = 'nose'
+    LEFT_EYE = 'left_eye'
+    RIGHT_EYE = 'right_eye'
+    LEFT_EAR = 'left_ear'
+    RIGHT_EAR = 'right_ear'
+    LEFT_SHOULDER = 'left_shoulder'
+    RIGHT_SHOULDER = 'right_shoulder'
+    LEFT_ELBOW = 'left_elbow'
+    RIGHT_ELBOW = 'right_elbow'
+    LEFT_WRIST = 'left_wrist'
+    RIGHT_WRIST = 'right_wrist'
+    LEFT_HIP = 'left_hip'
+    RIGHT_HIP = 'right_hip'
+    LEFT_KNEE = 'left_knee'
+    RIGHT_KNEE = 'right_knee'
+    LEFT_ANKLE = 'left_ankle'
+    RIGHT_ANKLE = 'right_ankle'
+
+
 class KeyPointsImagePlot(Enum):
     HEAD = 'head'
     NECK = 'neck'
