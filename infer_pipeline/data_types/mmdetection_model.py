c673484b614637d30dc976069ac1bd51f7da238d
lk <lk-mt-sc@proton.me>
2023-06-23 13:45:11 +0200

    Update key metric into key metric value and key metric name

--- infer_pipeline/data_types/mmdetection_model.py
+++ infer_pipeline/data_types/mmdetection_model.py
@@ -1,7 +1,8 @@
 class MMDetectionModel():
-    def __init__(self, name, key_metric, checkpoint, config):
+    def __init__(self, name, key_metric_value, key_metric_name, checkpoint, config):
         self.name = name
-        self.key_metric = key_metric
+        self.key_metric_value = key_metric_value
+        self.key_metric_name = key_metric_name
         self.checkpoint = checkpoint
         self.config = config
 
@@ -10,14 +11,17 @@ class MMDetectionModel():
         selection_str_split = selection_str.split(' | ')
         return MMDetectionModel(
             name=selection_str_split[0],
-            key_metric=selection_str_split[1],
+            key_metric_value=float(selection_str_split[1]),
+            key_metric_name=selection_str_split[2],
             checkpoint=None,
             config=None
         )
 
     def __str__(self):
-        return f'{self.name} | {self.key_metric}'
+        return f'{self.name} | {self.key_metric_value} | {self.key_metric_name}'
 
     def __eq__(self, other):
-        equal = self.name == other.name and self.key_metric == other.key_metric
+        equal = self.name == other.name \
+            and self.key_metric_value == other.key_metric_value \
+            and self.key_metric_name == other.key_metric_name
         return equal
--- infer_pipeline/data_types/mmpose_model.py
+++ infer_pipeline/data_types/mmpose_model.py
@@ -5,7 +5,8 @@ class MMPoseModel():
             arch,
             dataset,
             input_size,
-            key_metric,
+            key_metric_value,
+            key_metric_name,
             checkpoint,
             config,
             transfer_learned=False,
@@ -15,7 +16,8 @@ class MMPoseModel():
         self.arch = arch
         self.dataset = dataset
         self.input_size = input_size
-        self.key_metric = key_metric
+        self.key_metric_value = key_metric_value
+        self.key_metric_name = key_metric_name
         self.checkpoint = checkpoint
         self.config = config
         self.transfer_learned = transfer_learned
@@ -25,24 +27,26 @@ class MMPoseModel():
     def get_from_selection_string(cls, selection_str):
         selection_str_split = selection_str.split(' | ')
         return MMPoseModel(
-            section=selection_str_split[4],
-            arch=selection_str_split[5],
+            section=selection_str_split[5],
+            arch=selection_str_split[6],
             dataset=selection_str_split[1],
-            input_size=selection_str_split[3],
-            key_metric=selection_str_split[2],
+            input_size=selection_str_split[4],
+            key_metric_value=float(selection_str_split[2]),
+            key_metric_name=selection_str_split[3],
             checkpoint=None,
             config=None,
             transfer_learned=selection_str_split[0] == 'TL: True'
         )
 
     def __str__(self):
-        return f'TL: {str(self.transfer_learned)} | {self.dataset} | {self.key_metric} | {self.input_size} | {self.section} | {self.arch}'
+        return f'TL: {str(self.transfer_learned)} | {self.dataset} | {self.key_metric_value} | {self.key_metric_name} | {self.input_size} | {self.section} | {self.arch}'
 
     def __eq__(self, other):
         equal = self.section == other.section \
             and self.arch == other.arch \
             and self.dataset == other.dataset \
             and self.input_size == other.input_size \
-            and self.key_metric == other.key_metric \
+            and self.key_metric_value == other.key_metric_value \
+            and self.key_metric_name == other.key_metric_name \
             and self.transfer_learned == other.transfer_learned
         return equal
