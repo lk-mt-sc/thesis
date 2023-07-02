dd30b5d0300d5fb1af1f3353e7b38c1f82eab3c3
lk <lk-mt-sc@proton.me>
2023-07-02 22:03:41 +0200

    Update MMDetection models

--- infer_pipeline/manager/mmdetection_model_manager.py
+++ infer_pipeline/manager/mmdetection_model_manager.py
@@ -21,39 +21,43 @@ class MMDetectionModelManager():
         if not self.status_manager.has_status(Status.FETCHING_MMDETECTION_MODELS):
             self.status_manager.add_status(Status.FETCHING_MMDETECTION_MODELS)
 
-            # manually selected models based on box AP
             self.models.append(MMDetectionModel(
                 name='Faster R-CNN',
-                key_metric='box AP (0.937)',
-                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'non_deterministic_models',
-                                        'faster-rcnn', 'train', 'best_coco_bbox_mAP_epoch_29.pth'),
+                key_metric_value=0.921,
+                key_metric_name='box AP',
+                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'faster_rcnn',
+                                        'faster-rcnn', 'train', 'best_coco_bbox_mAP_epoch_14.pth'),
                 config=os.path.join(INFER_PIPELINE_MMDETECTION_CONFIGS_DIR, 'faster-rcnn.py')))
             self.models.append(MMDetectionModel(
                 name='RTMDet',
-                key_metric='box AP (0.804)',
-                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'deterministic_models', 'rtmdet',
-                                        'train', 'best_coco_bbox_mAP_epoch_44.pth'),
+                key_metric_value=0.952,
+                key_metric_name='box AP',
+                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'rtmdet',
+                                        'rtmdet', 'train', 'best_coco_bbox_mAP_epoch_37.pth'),
                 config=os.path.join(INFER_PIPELINE_MMDETECTION_CONFIGS_DIR, 'rtmdet.py')))
             self.models.append(MMDetectionModel(
                 name='YOLOX',
-                key_metric='box AP (0.884)',
-                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'deterministic_models', 'yolox',
-                                        'train', 'best_coco_bbox_mAP_epoch_250.pth'),
+                key_metric_value=0.947,
+                key_metric_name='box AP',
+                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'yolox', 'yolox',
+                                        'train', 'best_coco_bbox_mAP_epoch_62.pth'),
                 config=os.path.join(INFER_PIPELINE_MMDETECTION_CONFIGS_DIR, 'yolox.py')))
             self.models.append(MMDetectionModel(
                 name='VarifocalNet',
-                key_metric='box AP (0.731)',
-                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'non_deterministic_models', 'vfnet',
-                                        'train', 'best_coco_bbox_mAP_epoch_30.pth'),
+                key_metric_value=0.966,
+                key_metric_name='box AP',
+                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'vfnet',
+                                        'vfnet', 'train', 'best_coco_bbox_mAP_epoch_20.pth'),
                 config=os.path.join(INFER_PIPELINE_MMDETECTION_CONFIGS_DIR, 'vfnet.py')))
             self.models.append(MMDetectionModel(
                 name='TOOD',
-                key_metric='box AP (0.941)',
-                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'non_deterministic_models', 'tood',
-                                        'train', 'best_coco_bbox_mAP_epoch_27.pth'),
+                key_metric_value=0.966,
+                key_metric_name='box AP',
+                checkpoint=os.path.join(TRAIN_PIPELINE_MMDETECTION_TRAININGS_DIR, 'tood',
+                                        'tood', 'train', 'best_coco_bbox_mAP_epoch_22.pth'),
                 config=os.path.join(INFER_PIPELINE_MMDETECTION_CONFIGS_DIR, 'tood.py')))
-            
-            self.default_model = self.models[0]
+
+            self.default_model = self.models[3]
 
             self._gui_set_models()
             self.status_manager.remove_status(Status.FETCHING_MMDETECTION_MODELS)
@@ -65,7 +69,8 @@ class MMDetectionModelManager():
 
     def _gui_set_details(self):
         self.gui_mmdetection_model.details_name_var.set(self.selected_model.name)
-        self.gui_mmdetection_model.details_key_metric_var.set(self.selected_model.key_metric)
+        self.gui_mmdetection_model.details_key_metric_var.set(str(self.selected_model.key_metric_value)
+                                                              + ' ' + self.selected_model.key_metric_name)
         self.gui_mmdetection_model.details_checkpoint_var.set(self.selected_model.checkpoint)
         self.gui_mmdetection_model.details_config_var.set(self.selected_model.config)
 
